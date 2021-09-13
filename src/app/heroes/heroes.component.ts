4107ae466e51229bc5ffb26a8d804328e896d3c4
Erick Rincones <erick.rincones@ciens.ucv.ve>
2021-09-13 00:52:22 -0400

    Get Data from a Server

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,7 +13,10 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -23,6 +26,22 @@ export class HeroesComponent implements OnInit {
     this.heroService
       .getHeroes()
       .subscribe(heroes => { this.heroes = heroes; });
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+
+    this.heroService
+      .addHero({ name } as Hero)
+      .subscribe(hero => { this.heroes.push(hero); });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService
+      .deleteHero(hero.id)
+      .subscribe();
   }
 
 }
